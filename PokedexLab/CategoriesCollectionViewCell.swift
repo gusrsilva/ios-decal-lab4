5ef7ce438526a74ab2f4ce78667fb0daedde8cc2
Gustavo Silva <gus7silva@berkeley.edu>
2017-03-02 19:18:44 -0800

    Finish part 1

---
+++ PokedexLab/CategoriesCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CategoriesCollectionViewCell.swift
+//  PokedexLab
+//
+//  Created by Gus Silva on 3/2/17.
+//  Copyright © 2017 iOS Decal. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
--- PokedexLab/SearchViewController.swift
+++ PokedexLab/SearchViewController.swift
@@ -8,15 +8,18 @@
 
 import UIKit
 
-class SearchViewController: UIViewController {
+class SearchViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
     var pokemonArray: [Pokemon] = []
     var filteredArray: [Pokemon] = []
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         pokemonArray = PokemonGenerator.getPokemonArray()
-
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +37,36 @@ class SearchViewController: UIViewController {
         }
         return filtered
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let identifier = segue.identifier {
+            if identifier == "searchToCategory" {
+                if let dest = segue.destination as? CategoryViewController {
+                    dest.pokemonArray = filteredArray
+                }
+            }
+        }
+    }
+    
+    /*
+     *  Collection View Functions
+     */
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return PokemonGenerator.categoryDict.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "category", for: indexPath) as! CategoriesCollectionViewCell
+        if let imageTitle = PokemonGenerator.categoryDict[indexPath.item] {
+            cell.imageView.image = UIImage(named: imageTitle)
+        }
+        return cell
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        filteredArray = filteredPokemon(ofType: indexPath.item)
+        performSegue(withIdentifier: "searchToCategory", sender: self)
+    }
 
 }
